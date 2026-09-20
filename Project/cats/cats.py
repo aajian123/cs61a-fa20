44a295feefcd311f7a195e56325030c85c04b597
jian <jian43584@gmail.com>
2026-09-20 21:41:11 +0800

    finish Problem5

--- Project/cats/cats.py
+++ Project/cats/cats.py
@@ -97,7 +97,22 @@ def autocorrect(user_word, valid_words, diff_function, limit):
     than LIMIT.
     """
     # BEGIN PROBLEM 5
-    "*** YOUR CODE HERE ***"
+    min_diff = limit
+    min_word = ''
+    is_ini = 0
+    for i in range(0,len(valid_words)):
+        if user_word == valid_words[i]:
+            return user_word
+        elif diff_function(user_word,valid_words[i],limit) <= min_diff and is_ini == 0:
+            min_diff = diff_function(user_word,valid_words[i],limit)
+            min_word = valid_words[i]
+            is_ini = 1
+        elif diff_function(user_word,valid_words[i],limit) < min_diff and is_ini == 1:
+            min_diff = diff_function(user_word,valid_words[i],limit)
+            min_word = valid_words[i]
+    if is_ini == 0:
+        return user_word
+    return min_word 
     # END PROBLEM 5
 
 
