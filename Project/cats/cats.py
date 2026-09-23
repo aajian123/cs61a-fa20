4a90c5068e3c37ec63d9e803d63f8868b87ec839
jian <jian43584@gmail.com>
2026-09-23 09:28:24 +0800

    finish Problem 7

--- Project/cats/cats.py
+++ Project/cats/cats.py
@@ -137,24 +137,25 @@ def shifty_shifts(start, goal, limit):
 
 def pawssible_patches(start, goal, limit):
     """A diff function that computes the edit distance from START to GOAL."""
-    assert False, 'Remove this line'
-
-    if ______________: # Fill in the condition
+    if start == goal: # Fill in the condition
         # BEGIN
-        "*** YOUR CODE HERE ***"
+        return 0
         # END
-
-    elif ___________: # Feel free to remove or add additional cases
+    elif start == '' or goal == '':
+        return max(len(start),len(goal))
+    elif limit == 0:
+        return 1
+    elif start[0] == goal[0]: # Feel free to remove or add additional cases
         # BEGIN
-        "*** YOUR CODE HERE ***"
+        return pawssible_patches(start[1:],goal[1:],limit)
         # END
 
     else:
-        add_diff = ... # Fill in these lines
-        remove_diff = ...
-        substitute_diff = ...
+        add_diff = 1 + pawssible_patches(start,goal[1:],limit-1)
+        remove_diff = 1 + pawssible_patches(start[1:],goal,limit-1)
+        substitute_diff = 1 + pawssible_patches(start[1:],goal[1:],limit-1)
         # BEGIN
-        "*** YOUR CODE HERE ***"
+        return min(add_diff,remove_diff,substitute_diff)
         # END
 
 
